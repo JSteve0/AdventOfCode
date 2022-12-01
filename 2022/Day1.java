9875ff822ca23a9d25dca62346856150d5a6a83c
Justin <jmms5757@gmail.com>
2022-12-01 00:25:13 -0500

    finished day 1

---
+++ 2022/Day1.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Day1 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("./2022/input.txt");
+        Scanner console = new Scanner(file);
+
+        ArrayList<Integer> cal = new ArrayList<>();
+
+        int max = 0;
+        int curr = 0;
+        while (console.hasNextLine())
+        {
+            String temp = console.nextLine();
+            if (temp.equalsIgnoreCase(""))
+            {
+                cal.add(curr);
+                curr = 0;
+            } else {
+                curr += Integer.parseInt(temp);
+            }
+        }
+
+        cal.sort(Collections.reverseOrder());
+        System.out.println(cal.get(2) + cal.get(1) + cal.get(0));
+
+    }
+
+}
